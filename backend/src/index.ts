9c14e0516dd71ab3e8b6ad920855a23869b7c9da
Jeff Hackshaw <jhackshaw@microsoft.com>
2021-03-19 14:32:32 -0700

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,4 +10,4 @@ const server = build({
 
 // Start server
 server.listen(FASTIFY_PORT, "0.0.0.0");
-console.log(`🚀 IP Geo lookup service running on port ${FASTIFY_PORT}`);
+console.log(`rbacgrocerystore-api listening on port ${FASTIFY_PORT}`);
