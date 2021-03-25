442350a722b7725c7e0cc0f87bfc23d2568c7a1a
Jeff Hackshaw <jhackshaw@microsoft.com>
2021-03-25 09:57:50 -0700

    browser router with fallback route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, ProductPage } from "./pages";
 import { Layout } from "./components";
 import { UserContextProvider } from "./context";
